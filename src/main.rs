101431297cd7da2c894966a7f689371ac103bf16
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-09 13:15:52 +0000

    cargo: fmt

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,25 @@ fn main() {
     let mut s = String::new();
     io::stdin().read_line(&mut s).unwrap();
 
-    let mut array: Vec<i64> = s.split_whitespace().map(|d| d.parse::<i64>().unwrap()).collect();
+    let mut array: Vec<i64> = s
+        .split_whitespace()
+        .map(|d| d.parse::<i64>().unwrap())
+        .collect();
 
     array.shuffle(&mut rand::rng());
-        
+
     loop {
         array.shuffle(&mut rand::rng());
-        println!("{:?}",array);
+        println!("{:?}", array);
 
         //for items in 0..=array.len(){
-            //if array[items] < array[items+1]{
-                //println!("{:?}",array);
-            //}
+        //if array[items] < array[items+1]{
+        //println!("{:?}",array);
+        //}
         //}
         if array.is_sorted() {
             println!("Final array is:");
-            println!("{:?}",array);
+            println!("{:?}", array);
             break;
         }
     }
